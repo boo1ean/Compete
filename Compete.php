deae3ca86f9b5599d56288fd933e7e9dabd4a3c6
boo1ean <boo1ean0807@gmail.com>
2012-08-29 21:23:10 +0300

    Implement errors handling, add comments

--- Compete.php
+++ Compete.php
@@ -6,36 +6,101 @@ if (!function_exists('curl_init'))
 if (!function_exists('json_decode'))
   throw new Exception('Compete needs the JSON PHP extension.');
 
+/**
+ * Base Compete exception class.
+ */
+class CompeteException extends Exception {}
+
+/**
+ * Represents Compete API.
+ * @author Egor Gumenyuk (boo1ean0807 at gmail dot com)
+ * @package Compete
+ * @license Apache 2.0
+ */
 class Compete
 {
-  const API_BASE_URL = 'http://apps.compete.com/sites/:domain/trended/:metric/?apikey=:key';
-  const USER_AGENT = 'Compete API wrapper for PHP';
+  /**
+   * Default usr agent.
+   */
+  const USER_AGENT   = 'Compete API wrapper for PHP';
 
+  /**
+   * Base url for api calls.
+   */
+  const API_BASE_URL = 'http://apps.compete.com/sites/:domain/trended/:metric/?apikey=:key';
+
+  /**
+   * Masks for url params.
+   */
   private $_urlKeys = array(':domain', ':metric', ':key');
   private $_apiKey;
 
-  // For url cleaning
-  private $_toSearch   = array('http://', 'www.');
-  private $_toReplace  = array('', '');
+  /**
+   * For url cleaning.
+   */
+  private $_toSearch  = array('http://', 'www.');
+  private $_toReplace = array('', '');
 
+  /**
+   * List of available metrics.
+   */
+  private $_availableMetrics = array(
+              // Description      Auth type
+    'uv',     // Unique Visitors  Basic
+    'vis',    // Visits           Basic
+    'rank',   // Rank             Basic
+    'pv',     // Page Views       All-Access
+    'avgstay',// Average Stay     All-Access
+    'vpp',    // Visits/Person    All-Access
+    'ppv',    // Pages/Visit      All-Access
+    'att',    // Attention        All-Access
+    'reachd', // Daily Reach      All-Access
+    'attd',   // Daily Attention  All-Access
+    'gen',    // Gender           All-Access
+    'age',    // Age              All-Access
+    'inc',    // Income           All-Access
+  );
+
+  /**
+   * Create access to Compete API.
+   * @param string $apiKey user's api key.
+   */
   public function __construct($apiKey) {
     $this->_apiKey = $apiKey;
   }
 
+  /**
+   * Get data from Compete.
+   * @param string $site some domain.
+   * @param string $metric metric to get.
+   * @return stdClass Compete data.
+   * @throws CompeteException
+   */
   public function get($site, $metric) {
+    if (!in_array($metric, $this->_availableMetrics))
+      throw new CompeteException($metric . ' - wrong metric.');
+
     $values = array(
       $this->_prepareUrl($site),
       $metric,
       $this->_apiKey
     );
 
+    // Prepare call url
     $url = str_replace($this->_urlKeys, $values, self::API_BASE_URL);
-    return json_decode($this->_get($url));
+
+    // Retrieve data using HTTP GET method.
+    $data = json_decode($this->_get($url));
+
+    // Because of unsuccessful responses contain "status_message".
+    if (!isset($data->status_message))
+      return $data;
+
+    throw new CompeteException('Status: ' . $data->status . '. ' .$data->status_message);
   }
 
   /**
    * Cut unnecessary parts of url.
-   *
    * @param string $url some url.
    * @return string trimmed url.
    */
@@ -45,7 +110,6 @@ class Compete
 
   /**
    * Execute http get method.
-   *
    * @param string $url request url.
    * @return string response.
    */
